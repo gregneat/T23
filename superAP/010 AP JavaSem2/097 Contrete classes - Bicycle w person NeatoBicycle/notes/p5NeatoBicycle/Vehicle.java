d865e8af11779931db00c8475ca5cd7c6bac759c
gregneat <gneat@gusd.net>
2023-01-13 05:48:23 -0800

    added ap java sem2

---
+++ superAP/010 AP JavaSem2/097 Contrete classes - Bicycle w person NeatoBicycle/notes/p5NeatoBicycle/Vehicle.java
@@ -0,0 +1,18 @@
+package pkg;
+
+
+
+public abstract class Vehicle{
+
+	public abstract void setStep(double yeet);
+	public abstract void drive();
+	public abstract void translate(double x, double y);
+	public abstract void draw();
+	public abstract int getX();
+	public abstract int getY();
+	public abstract int getWidth();
+	public abstract int getHeight();
+
+
+
+}
